76948cc66a748650205faaa673c2103a8bf557c4
IsaacPilatuna <isapila1996@hotmail.com>
2018-12-09 17:11:43 -0500

    Lectura de Usuarios completa

--- Controllers/PersonaController.cs
+++ Controllers/PersonaController.cs
@@ -12,28 +12,77 @@ using System.Web;
 namespace SW_2.Controllers
 {
     [Route("api/[controller]")]
-
+   
     public class PersonaController : Controller
     {
-        public ActionResult Index()
-        {
+        public ActionResult Index(){
             return View();
         }
         [HttpGet]
-        public ActionResult EnviarCorre()
-        {
+        public ActionResult EnviarCorre(){
             return View();
         }
+        [HttpPost]
+        [Route("send")] 
+        public ActionResult EnviarCorreo(String para, String asunto, String mensaje){
+
+            try{
+                MailMessage correo= new MailMessage();
+                correo.From=new MailAddress("sofig.0106@hotmail.com");
+                correo.To.Add(para);
+                correo.Subject=asunto;
+                correo.Body=mensaje;
+                correo.IsBodyHtml=true;
+                correo.Priority=MailPriority.Normal;
+                SmtpClient smtp= new SmtpClient();
+                smtp.Host="smtp.gmail.com";
+                smtp.Port=25;
+                smtp.EnableSsl=true;
+                smtp.UseDefaultCredentials=true;
+                string sCuentaCorreo="sofig.0106@gmail.com";
+                string sPasswordCorreo="5109899555678";
+                smtp.Credentials=new System.Net.NetworkCredential(sCuentaCorreo,sPasswordCorreo);
+                smtp.Send(correo);
+                ViewBag.mensaje="mensaje enviador";
+
+            }catch(Exception ex){
+
+            }
+            return View();
+
+        }
+
+        
+        baseswContext context=new baseswContext();
+        
+
+        [HttpPost]
+        [Route("Add")] 
+        public List<Persona> Lista([FromBody]Persona temp){
+            Persona persona= new Persona{
+                Nombrecompleto=temp.Nombrecompleto,
+                Identificacionpersonal=temp.Identificacionpersonal,
+                Idtipopersona=temp.Idtipopersona,
+                Correo=temp.Correo
+                
+            };
+            context.Persona.Add(persona);
+            context.SaveChanges();
+            return this.context.Persona.ToList();
+        }
+
         public class JoinPersona
         {
             public int Idpersona { get; set; }
             public string Nombrecompleto { get; set; }
             public string Identificacionpersonal { get; set; }
+            public string Correo { get; set; }
             public int Idtipopersona { get; set; }
             public string Nombre { get; set; }
         }
-        [HttpGet]
 
+        
+        [HttpGet]
         [Route("InfoPersonas")]
         public List<JoinPersona> Lista()
         {
@@ -45,6 +94,7 @@ namespace SW_2.Controllers
                             Idpersona = persona.Idpersona,
                             Nombrecompleto = persona.Nombrecompleto,
                             Identificacionpersonal = persona.Identificacionpersonal,
+                            Correo = persona.Correo,
                             Idtipopersona = persona.Idtipopersona,
                             Nombre = tipoPersona.Nombre
                         };
@@ -52,58 +102,9 @@ namespace SW_2.Controllers
             List<JoinPersona> lista = query.ToList();
             return lista;
         }
-        [HttpPost]
-        [Route("send")]
-        public ActionResult EnviarCorreo(String para, String asunto, String mensaje)
-        {
-
-            try
-            {
-                MailMessage correo = new MailMessage();
-                correo.From = new MailAddress("sofig.0106@hotmail.com");
-                correo.To.Add(para);
-                correo.Subject = asunto;
-                correo.Body = mensaje;
-                correo.IsBodyHtml = true;
-                correo.Priority = MailPriority.Normal;
-                SmtpClient smtp = new SmtpClient();
-                smtp.Host = "smtp.gmail.com";
-                smtp.Port = 25;
-                smtp.EnableSsl = true;
-                smtp.UseDefaultCredentials = true;
-                string sCuentaCorreo = "sofig.0106@gmail.com";
-                string sPasswordCorreo = "5109899555678";
-                smtp.Credentials = new System.Net.NetworkCredential(sCuentaCorreo, sPasswordCorreo);
-                smtp.Send(correo);
-                ViewBag.mensaje = "mensaje enviador";
-
-            }
-            catch (Exception ex)
-            {
-
-            }
-            return View();
-
-        }
-
-
-        baseswContext context = new baseswContext();
-        [HttpPost]
-        [Route("Add")]
-        public List<Persona> Lista([FromBody]Persona temp)
-        {
-            Persona persona = new Persona
-            {
-                Nombrecompleto = temp.Nombrecompleto,
-                Identificacionpersonal = temp.Identificacionpersonal,
-                Idtipopersona = temp.Idtipopersona,
-
-
-            };
-            context.Persona.Add(persona);
-            context.SaveChanges();
-            return this.context.Persona.ToList();
-        }
+        
     }
 
+    
+        
 }

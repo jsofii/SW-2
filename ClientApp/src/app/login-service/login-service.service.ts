26fe48a930917a5808bc92c8c016ce000ae3369d
jsofii <sofi-0106@hotmail.com>
2018-12-08 23:22:00 -0500

    componentes horario y reserva

---
+++ ClientApp/src/app/login-service/login-service.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { HttpHeaders } from '@angular/common/http';
+import { MateriaComponent } from '../materia/materia.component';
+import { Observable } from 'rxjs/Observable';
+
+
+
+
+@Injectable()
+export class loginServiceService {
+
+
+  constructor(private http: HttpClient) {
+    this.host = "https://localhost:5001";
+  }
+
+  host: string;
+
+
+  ListaTodasMaterias() {
+    return this.http.get('https://localhost:5001/api/Materia/ListaMaterias');
+
+  }
+  
+  AddMateria(nombreMateria: any, codMateria: any) {
+    var temp = {
+      Nombre: nombreMateria,
+      Codigo: codMateria
+      
+    }
+
+    
+    return this.http.post('https://localhost:5001/api/Materia/Addmat/', temp);
+
+  }
+
+  DeleteMateria(idmateria: number){
+    return this.http.delete('https://localhost:5001/api/Materia/Eliminar/' + idmateria);
+  
+  }
+
+  
+
+
+  
+}

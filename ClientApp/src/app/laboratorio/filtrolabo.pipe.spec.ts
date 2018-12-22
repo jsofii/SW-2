eeabd7b5c7d9305bd29fc9ec646a22b7e9598f28
cabp1996 <41761406+cabp1996@users.noreply.github.com>
2018-12-21 21:48:56 -0500

    BusquedaFiltradaMateriaLabo
    BusquedaFiltradaMateriaLabo

---
+++ ClientApp/src/app/laboratorio/filtrolabo.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FiltrolaboPipe } from './filtrolabo.pipe';
+
+describe('FiltrolaboPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FiltrolaboPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ ClientApp/src/app/materia/pipemateria.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PipemateriaPipe } from './pipemateria.pipe';
+
+describe('PipemateriaPipe', () => {
+  it('create an instance', () => {
+    const pipe = new PipemateriaPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
